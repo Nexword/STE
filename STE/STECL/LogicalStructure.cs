c52a3502819bdbd537f4c178e40c2fbbac8f453b
Nexword <Anatrim@yandex.ru>
2014-01-06 11:03:38 +0400

    Проверка

--- STE/STECL/LogicalStructure.cs
+++ STE/STECL/LogicalStructure.cs
@@ -6,5 +6,28 @@ using System.Threading.Tasks;
 
 namespace STECL
 {
+    interface ISTETest { }
+    interface ISTEAnswerValue { }
 
+    public class STESimpleTest:ISTETest {}
+
+    public class STETestNote { }
+
+    public class STETaskBlock { }
+
+    public class STETask { }
+
+    public class STEQuestion { }
+
+    public class STEAnswer : ISTEAnswerValue { }
+#region STEAnswer region
+    public class STEAnswerOption { }
+    public class STEAnswerSingleChoice : STEAnswer { }
+    public class STEAnswerMultipleChoice : STEAnswer { }
+    public class STEAnswerMatching : STEAnswer { }
+    public class STEAnswerOpenText : STEAnswer { }
+#endregion
+#region STEAnswerValue region
+    public class STEAnswerValueSingleChoice : ISTEAnswerValue { }
+#endregion
 }
