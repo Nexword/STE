63a6237a6b8fdba5cdac8a84e9849335f93c24c6
Nexword <Anatrim@yandex.ru>
2014-01-06 11:48:48 +0400

    Что-то я туплю

--- STE/STECL/LogicalStructure.cs
+++ STE/STECL/LogicalStructure.cs
@@ -9,17 +9,38 @@ namespace STECL
     interface ISTETest { }
     interface ISTEAnswerValue { }
 
-    public class STESimpleTest:ISTETest {}
+    public class STESimpleTest:ISTETest 
+    {
+        private int id;
+        public int Id { get; set; }
+        List<STETaskBlock> TaskBlock = new List<STETaskBlock>();
+    }
 
     public class STETestNote { }
 
-    public class STETaskBlock { }
+    public class STETaskBlock 
+    {
+        private int id;
+        public int Id { get; set; }
+        List<STETask> Task = new List<STETask>();
+    }
 
-    public class STETask { }
+    public class STETask 
+    {
+        private int id;
+        public int Id { get; set; }
+        public STEQuestion Question;
+        List<STEAnswer> Task = new List<STEAnswer>();
+    }
 
     public class STEQuestion { }
 
-    public class STEAnswer : ISTEAnswerValue { }
+    public class STEAnswer  
+    {
+        private int id;
+        public int Id { get; set; }
+        public ISTEAnswerValue GetUserAnswer();
+    }
 #region STEAnswer region
     public class STEAnswerOption { }
     public class STEAnswerSingleChoice : STEAnswer { }
@@ -29,5 +50,8 @@ namespace STECL
 #endregion
 #region STEAnswerValue region
     public class STEAnswerValueSingleChoice : ISTEAnswerValue { }
+    public class STEAnswerValueMultipleChoice : ISTEAnswerValue { }
+    public class STEAnswerValueMatching : ISTEAnswerValue { }
+    public class STEAnswerValueOpenText : ISTEAnswerValue { }
 #endregion
 }
