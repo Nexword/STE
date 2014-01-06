4c8fa5d20f2aacb3a5d4dfa72f374691f3b4ec4c
haganeaclhemist <haganenofullmetal@gmail.com>
2014-01-06 12:09:29 +0400

    WTF?! Network added! Blah Blah Blah

---
+++ STE/STEIL/Network.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace STEIL
+{
+    #region client
+    public class STENetworkClient
+    {
+        private string hostname;
+        private int port;
+        private TcpClient connection; 
+        public STENetworkClient(string name, int _port)
+        {
+            hostname=name;
+            port=_port;
+        }
+        public int Connect()
+        {
+            return 0;
+        }
+        public int Authorize()
+        {
+            return 0;
+        }
+        public void RequireTestList()
+        {
+
+        }
+        public void RequireTest()
+        {
+
+        }
+        public void CheckResults()
+        {
+
+        }
+        public void GetReport(string[] rules)
+        {
+
+        }
+        public void Disconnect()
+        {
+            connection.Close();
+        }
+    }
+#endregion client
+    #region server
+    public class STENetworkServer
+    {
+
+    }
+    public class STEClientConnection
+    {
+
+    }
+#endregion server
+}
